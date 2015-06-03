3dc99847d978f01d2265cabfe235f3b5fbe2363b
Smirnov Alexander <alllecs@yandex.ru>
2015-06-03 11:51:31 +0300

    Изменено соотношение ширины колонок frame

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
 		<title>My site</title>
 	</head>
 
-	<frameset cols="20%, 80%" border="0">
+	<frameset cols="12%, 88%" border="0">
 	<frameset rows="20%, 80%" border="0">
 	<frame src="https://www.niisi.ru/PAH2015.png" align="middle" alt="logo" width="100" height="500" scrolling="no" marginwidth="0" marginheight="0" noresize>
 	<frame src="menu.php">
