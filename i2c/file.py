3ac77df0a384feb15c837795f4958855c1a6b691
Alexander Smirnov <alllecs@yandex.ru>
2015-09-02 11:28:50 +0300

    Исправлен вывод часов на дисплей по i2c

--- i2c/file.py
+++ i2c/file.py
@@ -19,12 +19,17 @@ lcd = lcddriver.lcd()
 
 lcd.lcd_clear()
 
-f1 = open('/sys/class/gpio/gpio5/value')
+def newfunc(gpionum):
+	f1 = open('/sys/class/gpio/gpio%d/value' % gpionum)
+	# "gpio%d/value" % (gpionum,)
+	return f1.readline(1)
+
+#f1 = open('/sys/class/gpio/gpio5/value')
 f2 = open('/sys/class/gpio/gpio6/value')
 f3 = open('/sys/class/gpio/gpio13/value')
 f4 = open('/sys/class/gpio/gpio19/value')
 
-lcd.lcd_display_string("Relay 1: " + f1.readline(1) + " Relay 2:" + f2.readline(1), 1)
+lcd.lcd_display_string("Relay 1: " + newfunc(5) + " Relay 2:" + f2.readline(1), 1)
 lcd.lcd_display_string("Relay 3: " + f3.readline(1) + " Relay 4:" + f4.readline(1), 2)
 
 date_str = time.strftime("%Y %b %d %H:%M")
