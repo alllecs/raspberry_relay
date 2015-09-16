e9e3f9214441df77c41ddbfe70f2288b2fcfb846
Alexander Smirnov <alllecs@yandex.ru>
2015-09-16 14:44:03 +0300

    Добавлен раздел с камерой на веб сайте

--- web/index.php
+++ web/index.php
@@ -9,9 +9,9 @@
 	<frameset cols="12%, 88%" border="0">
 	<frameset rows="20%, 80%" border="0">
 	<frame src="https://www.niisi.ru/PAH2015.png" align="middle" alt="logo" width="100" height="500" scrolling="no" marginwidth="0" marginheight="0" noresize>
-	<frame src="menu.php">
+	<frame src="web/menu.php">
 	</frameset>
-	<frame src="home.php" name="window-1">
+	<frame src="web/home.php" name="window-1">
 	</frameset>
 	
 </html>
--- web/menu.php
+++ web/menu.php
@@ -11,6 +11,7 @@
 			<li><a href="power.php" target="window-1">Питание</a></li>
 			<li><a href="status.php" target="window-1">Состояние</a></li>
 			<li><a href="info.php" target="window-1">Информация</a></li>
+			<li><a href="cam.php" target="window-1">Камера</a></li>
 		</ol>
 	</body>
 </html>
