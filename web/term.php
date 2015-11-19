f751918de247ff48f9f79757f11b2b7749cb077c
Alexander Smirnov <alllecs@yandex.ru>
2015-11-19 16:45:06 +0300

    Добавлена консоль и файл терминала в web-интерфейсе

---
+++ web/term.php
@@ -0,0 +1,23 @@
+<html>
+        <head>
+                <meta charset="utf-8">
+                <title>Terminal</title>
+                <style>
+                table {
+                        width: 1000px;
+                        border: 1px solid black;
+                        margin: auto;
+                }
+                td {
+                        text-align: center;
+                }
+                </style>
+        </head>
+
+        <body text="0000CC" bgcolor="#F5F5F5" link="#33FF00" alink="#CC0099" vli
+nk="#00FFFF">
+
+        <img src="http://192.168.1.2:8022">
+
+        </body>
+</html>
