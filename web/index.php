002d7eed4eb73bc40ed453cd845d05401b26d0ef
Alexander Smirnov <alllecs@yandex.ru>
2015-11-24 15:45:58 +0300

    Исправлен вывод при уменьшении окна браузера

--- web/index.php
+++ web/index.php
@@ -5,11 +5,8 @@
 		<title>My site</title>
 	</head>
 
-	<frameset cols="12%, 88%" border="0">
-	<frameset rows="20%, 80%" border="0">
-	<frame src="https://www.niisi.ru/PAH2015.png" align="middle" alt="logo" width="100" height="500" scrolling="no" marginwidth="0" marginheight="0" noresize>
-	<frame src="menu.php">
-	</frameset>
+	<frameset cols="20%, *" border="0">
+	<frame src="menu.php" min-width="15%" noresize>
 	<frame src="home.php" name="window-1">
 	</frameset>
 	
--- web/menu.php
+++ web/menu.php
@@ -5,14 +5,17 @@
 	</head>
 
 	<body bgcolor="#F5F5F5">
-
-		<ol start="1">
-			<li><a href="home.php" target="window-1">Главная</a></li>
-			<li><a href="power.php" target="window-1">Питание</a></li>
-			<li><a href="status.php" target="window-1">Состояние</a></li>
-			<li><a href="info.php" target="window-1">Информация</a></li>
-			<li><a href="cam.php" target="window-1">Камера</a></li>
-			<li><a href="http://localhost:8022" target="window-1">Терминал</a></li>
-		</ol>
+	<tr>
+	<p><a href="https://www.niisi.ru/"><img src="https://www.niisi.ru/PAH2015.png" width="100" height="100" alt="NIISI RAN"></a></p>
+        <td align="left" bgcolor="#99CCFF" height="25" valign="top" width="20">
+        <font face="Times" size="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <li><a href="home.php" target="window-1">Главная</a></li>
+                <li><a href="power.php" target="window-1">Питание</a></li>
+                <li><a href="status.php" target="window-1">Состояние</a></li>
+                <li><a href="info.php" target="window-1">Информация</a></li>
+                <li><a href="cam.php" target="window-1">Камера</a></li>
+                <li><a href="console/console.php" target="window-1">Терминал</a></li>
+        </font></td></tr>
+        </font></td>
 	</body>
 </html>
